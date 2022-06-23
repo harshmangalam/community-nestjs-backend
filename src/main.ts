65df3bb2a2bf492920304cd1802635c704a306fb
Harsh Mangalam <hmangalam@oplglobal.com>
2022-06-23 10:50:44 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(4000);
 }
